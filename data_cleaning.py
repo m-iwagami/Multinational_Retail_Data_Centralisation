9d473b465b8c918a62ffb6049755ddd8848b0b21
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-07-27 22:51:14 +0100

    Milestone3

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from geopy.geocoders import Nominatim
+
 from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
 import re
@@ -7,57 +9,83 @@ import re
 class DataCleaning:
     
     def clean_user_data(self, df):
-        df = df[df['date_of_birth'] != 'NULL']
-        df_alph_columns = ['first_name', 'last_name', 'country']
-        df = self.clean_letters(df, df_alph_columns)
+        df = df.replace(['NaN', 'NULL','NaT'], np.nan)
+        df = df.dropna()
+        df = self.clean_letters(df, 'first_name')
+        df = self.clean_letters(df, 'last_name')
+        df = self.clean_letters(df, 'country')
         df = self.clean_country_code(df, 'country_code')
         df = self.clean_email(df, 'email_address')
-        df = self.clean_uuid(df, 'user_uuid')
-        df = self.clean_address(df, 'address')
         df['date_of_birth'] = self.fixing_date(df,'date_of_birth')
         df['join_date'] = self.fixing_date(df,'join_date')
+
+        df = self.clean_uuid(df, 'user_uuid')
         df = self.clean_phone_number(df, 'phone_number','country_code')
+        #df = self.clean_address(df, 'address')
         return df
 
     
     def called_clean_store_data(self,df):
 
-
-        df = df[df['address'] != 'NULL']
+        df = df[df['store_code'] != 'NULL']
+        #df = self.clean_code(df, 'store_code')
         df = self.clean_country_code(df, 'country_code')
         df['opening_date'] = self.fixing_date(df,'opening_date')
-        df_alph_columns = ['store_type', 'locality']
-        df = self.clean_letters(df, df_alph_columns)
-        df = self.clean_address(df, 'address')
-        df = self.clean_store_code(df, 'store_code')
-        df = self.clean_numbers(df, 'staff_numbers')
+        
+        df = self.clean_letters(df, 'store_type')
+        df = self.clean_letters(df, 'locality')
+        df = self.clean_continent(df, 'continent')
         df = self.clean_latitude(df, 'latitude')
         df = self.clean_longitude(df, 'longitude')
-        df = self.clean_continent(df, 'continent')
+
+        df = self.clean_numbers(df, 'staff_numbers')
+        #df = self.clean_address(df, 'address')
         df = pd.DataFrame(df)
         return df
 
+
     
     def clean_products_data(self, df):
-        df = df.replace(['NaN', 'NULL'], np.nan)
+        df = df.replace(['NaN', 'NULL','NaT'], np.nan)
         df = df.dropna()
+    
         df = self.clean_price(df,'product_price')
         df['date_added'] = self.fixing_date(df, 'date_added')
         df = self.clean_ean_number(df, 'EAN')
         df = self.convert_product_weights(df, 'weight')
-
+#
         return df
     
+    def clean_card_data(self,df):
+        df = df[df['card_number'] != 'NULL']
+        df = self.clean_card_number(df, 'card_number')
+        df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], format='%Y-%m-%d', errors='coerce')
+        return df
+
+    def clean_card_number(self,df,column):
+        df[column] = df[column].astype(str)
+        df[column] = df[column].str.replace('?','')
+        card_pattern = r'^[0-9]{9,20}'
+        return df[df[column].str.match(card_pattern, na=False)]
+
+
+        #df[column] = df[column].match(card_pattern)
+        #df = df[df['card_number'].str.len().between(12,19)]
+        #df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%d', errors='coerce')
+    
     def clean_data_details(self, df):
-        df = df.replace(['Nan','NULL'], np.nan)
+        df = df.replace(['Nan','NULL','N/A'], np.nan)
         df = df.dropna()
         df = self.clean_numbers(df,'month')
         return df
     
 
-    def clean_letters(self,df,column):
-        return df[df[column].apply(lambda x:x.str.match(r'^[a-zA-Z\s]+$')).all(axis=1)] 
-    
+    def clean_letters(self, df, column):
+        df = df.replace(['Nan','NULL','N/A'], np.nan)
+        df[column] = df[column].astype(str)
+        return df[df[column].str.contains(r'^[a-zA-Z\s-]*$', na=False)]
+        
+
     def clean_country_code(self, df, column):
         country_code = r'\b[A-Z]{2}\b'
         return df[df[column].str.match(country_code, na=False)]
@@ -95,59 +123,85 @@ class DataCleaning:
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         return df[df[columns].str.match(regex, na=False)]
 
+    #def clean_address(self, df, column):
+     #   return df[df[column].apply(lambda x: pd.notna(x) and re.match(r'^[a-zA-Z0-9\s\.\,\\\ä\ö\ü\ß]+$', x) is not None)]
+
+ 
     def clean_address(self, df, column):
-        return df[df[column].apply(lambda x: re.match(r'^[a-zA-Z0-9\s\.\,\\\ä\ö\ü\ß]+$', x) is not None)]
+        # Create a copy of the DataFrame to avoid modifying the original data
+        cleaned_df = df.copy()
 
-    def clean_uuid(self, df, columns):
+        # Initialize the Nominatim geocoder
+        geolocator = Nominatim(user_agent="address_cleaner")
+
+        # Clean the address column
+        for index, address in enumerate(cleaned_df[column]):
+            if pd.notna(address):
+                location = geolocator.geocode(address, addressdetails=True, language="en")
+                if location and 'address' in location.raw:
+                    address_parts = location.raw['address']
+                    cleaned_address = ', '.join(filter(lambda x: x.strip(), [address_parts.get('road', ''), address_parts.get('house_number', ''), address_parts.get('city', ''), address_parts.get('state', ''), address_parts.get('postcode', ''), address_parts.get('country', '')]))
+                    cleaned_df.at[index, column] = cleaned_address
+
+        return cleaned_df
+
+
+
+    def clean_uuid(self, df, column):
+        df[column] = df[column].astype(str)
         id_pattern = r'^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}'
-        return df[df[columns].str.match(id_pattern, na=False)]
-
-    def clean_card_data(self,df):
-        df = df[df['card_number'] != 'NULL']
-        df = df[df['card_number'].str.len().between(16,19)]
-        #df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%d', errors='coerce')
-        df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], format='%Y-%m-%d', errors='coerce')
+        return df[df[column].str.match(id_pattern, na=False)]
+    
+    
+    def clean_code(self, df, column):
+        id_pattern = r'^[a-zA-Z]{2,3}-[a-zA-Z0-9]{6,8}'
+        df[column] = np.where(df[df[column].str.match(id_pattern)], df[column], np.nan)
         return df
     
-    def clean_store_code(self, df, columns):
-        id_pattern = r'^[a-zA-Z]{2}-[a-zA-Z0-9]{8}'
-        return df[df[columns].str.match(id_pattern, na=False)]
-    
     def clean_numbers(self, df, column):
-        return df[df[column].str.isdigit()]
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        return df
+    
 
     def clean_continent(self, df, column):
         continents = ['Asia', 'Africa', 'America', 'South America', 'Europe','Antarctica', 'Australia']
-        return df[df[column].isin(continents)]
+        df[column] = df[column].str.replace('eeEurope','Europe')
+        df[column] = df[column].str.replace('eeAmerica','America')
+        return df[df[column].isin(continents) | df[column].isnull()]
     
     def clean_latitude(self, df, column):
         df[column] = pd.to_numeric(df[column], errors='coerce')
-        return df[df[column].apply(lambda x: -90 <= x <= 90)]
-
+        df[column] = np.where((df[column] >= -90), df[column], np.nan )
+        return df
+    
     def clean_longitude(self, df, column):
         df[column] = pd.to_numeric(df[column], errors='coerce')
-        return df[df[column].apply(lambda x: -180 <= x <= 180)]
+        return df[(df[column] >= -180) & (df[column] <= 180) | pd.isna(df[column])]
     
-        
     def clean_price(seld, df, column):
         pattern = r'^£(\d+(\.\d{2}))$'
-        clean_price = df[column].str.match(pattern, na=False)
-        return df[clean_price]
+        df[column] = np.where(df[column].str.match(pattern), df[column], np.nan)
+        df = df.dropna(subset='product_price')
+
+        return df
 
     def clean_ean_number(self, df, column):
-        df = df[df[column].str.len() ==13]
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        df[column] = np.where(len(df[column]) <= 13, df[column], np.nan)
         return df
 
 
 
     def convert_product_weights(self, df, column):
+        df[column] = df[column].str.replace(r'^\d+\s[x]\s\d+', '0', regex=True)
+
         kg_rows = df[column].str.match(r'^\d+(\.\d+)?\s*kg$')
         g_rows = df[column].str.match(r'^\d+(\.\d+)?\s*g$')
         ml_rows = df[column].str.endswith('ml')
         
         df[column] = df[column].str.replace('[^\d.]', '', regex=True)
-        df[column] = df[column].astype(float)
-
+        df[column] = df[column].astype(float, errors='ignore')
+#
         df.loc[ml_rows, column] = df.loc[ml_rows, column] / 1000
         df.loc[g_rows, column] = df.loc[g_rows, column] / 1000
         df.loc[kg_rows, column] = df.loc[kg_rows, column]
